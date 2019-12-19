63c5a9e6fb5ed630793f3d35d6240b1edad0b72d
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2019-12-18 20:30:18 -0500

    keys debug test2

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ if(process.env.NODE_ENV === 'production') {
     console.log("we're in production");
     module.exports = require('./prod');
 }else{
+    console.log("we're not in production :(");
    module.exports =  require('./dev');
 }
