e21b37ce35b3a55adada6bdc7e8de25c163c607a
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2019-12-20 13:17:13 -0500

    whitelisted ip

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,7 @@ passport.use(new GoogleStrategy({
 
 }, function(accessToken, refreshToken, profile
     ,done){  
-        
+        console.log('testing')
         User.findOne({googleID: profile.id}).then(existingUser =>{
             console.log('findingUser');    
             if(existingUser){
